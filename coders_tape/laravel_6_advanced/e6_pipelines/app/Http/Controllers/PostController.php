52e1d15cc4357e9029a98a9bfc09f6816db2e6aa
Timothy Karani <kartimothy@gmail.com>
2022-06-10 22:13:35 +0300

    Can apply different filters

--- coders_tape/laravel_6_advanced/e6_pipelines/app/Http/Controllers/PostController.php
+++ coders_tape/laravel_6_advanced/e6_pipelines/app/Http/Controllers/PostController.php
@@ -12,7 +12,15 @@ class PostController extends Controller
     $posts = Post::query();
 
     if (request()->has('active')) {
+      // localhost:8000/?active=1
       $posts->where('active', request('active'));
+    }
+
+
+    if (request()->has('sort')) {
+      // localhost:8000/?sort=asc
+      // localhost:8000/?sort=desc
+      $posts->orderBy('title', request('sort'));
     }
 
     $posts = $posts->get();
