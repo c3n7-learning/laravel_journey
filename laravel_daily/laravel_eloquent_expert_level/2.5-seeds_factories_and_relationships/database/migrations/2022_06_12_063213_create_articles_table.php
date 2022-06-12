a4fcb041cbbed7aecf496db6630c3b74cd1fd10b
Timothy Karani <kartimothy@gmail.com>
2022-06-12 11:27:41 +0300

    Made field nullable

--- laravel_daily/laravel_eloquent_expert_level/2.5-seeds_factories_and_relationships/database/migrations/2022_06_12_063213_create_articles_table.php
+++ laravel_daily/laravel_eloquent_expert_level/2.5-seeds_factories_and_relationships/database/migrations/2022_06_12_063213_create_articles_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
       $table->id();
       $table->string("title");
       $table->text("article_text");
-      $table->foreignId("user_id")->constrained("users");
+      $table->foreignId("user_id")->nullable()->constrained("users");
       $table->timestamps();
     });
   }
