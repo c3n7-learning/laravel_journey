493410e483815b40ad37d4462f2136786ad41c31
Timothy Karani <kartimothy@gmail.com>
2022-06-12 11:03:56 +0300

    Added accessor

--- laravel_daily/laravel_eloquent_expert_level/2.3-accessors_mutators/app/Models/Article.php
+++ laravel_daily/laravel_eloquent_expert_level/2.3-accessors_mutators/app/Models/Article.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -18,6 +20,12 @@ class Article extends Model
     "id"
   ];
 
-
   protected $perPage = 10;
+
+  protected function randomTitle(): Attribute
+  {
+    return Attribute::make(
+      get: fn () => Str::kebab($this->title . " " . Str::random(4)),
+    );
+  }
 }
