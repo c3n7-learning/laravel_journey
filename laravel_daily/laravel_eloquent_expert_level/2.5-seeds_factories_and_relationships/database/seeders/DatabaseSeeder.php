9d6c0aeac77cc78c2dac3686142a659ff9f95ce3
Timothy Karani <kartimothy@gmail.com>
2022-06-12 11:21:50 +0300

    First of many

---
+++ laravel_daily/laravel_eloquent_expert_level/2.5-seeds_factories_and_relationships/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+
+    $this->call(UserTableSeeder::class);
+
+    \App\Models\Article::factory(1000)->create();
+  }
+}
