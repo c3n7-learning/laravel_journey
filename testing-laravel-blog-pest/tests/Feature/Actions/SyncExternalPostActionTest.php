2a2753fc752b1aa35c72a9f3ad574a42d4a78985
Timothy Karani <kartimothy@gmail.com>
2022-07-20 08:35:01 +0300

    Can test using Http facade

--- testing-laravel-blog-pest/tests/Feature/Actions/SyncExternalPostActionTest.php
+++ testing-laravel-blog-pest/tests/Feature/Actions/SyncExternalPostActionTest.php
@@ -2,23 +2,43 @@
 
 use App\Actions\SyncExternalPostAction;
 use App\Models\ExternalPost;
-use App\Support\Rss\FakeRssRepository;
+use App\Support\Rss\RssEntry;
+use App\Support\Rss\RssRepository;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Http;
 
 use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
 
 it('will sync an external feed to the database', function () {
   /** @var Illuminate\Foundation\Testing\TestCase $this */
 
   // arrange
-  $rssRepository = new FakeRssRepository();
+  Http::fake([
+    'https://example.com/*' => Http::sequence()
+      ->push(getFeed('test-a'))
+      ->push(getFeed('test-b'))
+  ]);
 
   // act
-  $syncExternalPostAction = new SyncExternalPostAction($rssRepository);
+  $syncExternalPostAction = app(SyncExternalPostAction::class); // let laravel inject the RssRepository
   $syncExternalPostAction('https://example.com/feed');
 
   // assert
   assertDatabaseHas(ExternalPost::class, [
-    'url' => 'https://test.com',
-    'title' => 'test',
+    'url' => 'https://example.com',
+    'title' => 'test-a',
+  ]);
+
+  assertDatabaseMissing(ExternalPost::class, [
+    'url' => 'https://example.com',
+    'title' => 'test-b',
+  ]);
+
+  $syncExternalPostAction('https://example.com/feed');
+
+  assertDatabaseHas(ExternalPost::class, [
+    'url' => 'https://example.com',
+    'title' => 'test-b',
   ]);
 });
