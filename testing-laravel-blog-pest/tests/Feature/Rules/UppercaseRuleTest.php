ae3b3afbcb57d10458edddeafd74dec1248f3eac
Timothy Karani <kartimothy@gmail.com>
2022-07-20 11:57:49 +0300

    Added datasets

--- testing-laravel-blog-pest/tests/Feature/Rules/UppercaseRuleTest.php
+++ testing-laravel-blog-pest/tests/Feature/Rules/UppercaseRuleTest.php
@@ -2,18 +2,14 @@
 
 use App\Rules\UppercaseRule;
 
-it('will pass for upper cased values', function () {
+it('will pass for upper cased values', function (string $value, bool $expectedResult) {
   /** @var Illuminate\Foundation\Testing\TestCase this */
 
-  $result = (new UppercaseRule())->passes('name', 'my string');
-  expect($result)->toBeFalse();
-
-  $result = (new UppercaseRule())->passes('name', 'MY STRING');
-  expect($result)->toBeTrue();
-
-  $result = (new UppercaseRule())->passes('name', 'MY STRINg');
-  expect($result)->toBeFalse();
-
-  $result = (new UppercaseRule())->passes('name', 'My string');
-  expect($result)->toBeFalse();
-});
+  $result = (new UppercaseRule())->passes('name', $value);
+  expect($result)->toBe($expectedResult);
+})->with([
+  ['MY STRING', true],
+  ['my string', false],
+  ['MY STRINg', false],
+  ['My string', false],
+]);
