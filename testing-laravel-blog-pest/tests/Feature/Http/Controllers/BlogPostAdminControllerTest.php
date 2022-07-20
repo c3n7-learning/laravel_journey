5c3345ea2e9bd47d63b6c94a21c1464b4b2ca39f
Timothy Karani <kartimothy@gmail.com>
2022-07-20 11:37:19 +0300

    Refactored test

--- testing-laravel-blog-pest/tests/Feature/Http/Controllers/BlogPostAdminControllerTest.php
+++ testing-laravel-blog-pest/tests/Feature/Http/Controllers/BlogPostAdminControllerTest.php
@@ -7,16 +7,19 @@ use App\Models\User;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
+beforeEach(function () {
+  $this->post = BlogPost::factory()->create();
+});
+
 it('will update a blog post when an admin is logged in', function () {
   /** @var Illuminate\Foundation\Testing\TestCase $this */
 
-  $post = BlogPost::factory()->create();
 
-  $sendRequest = fn () => post(action([BlogPostAdminController::class, 'update'], $post->slug), [
+  $sendRequest = fn () => post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
     'title' => 'test',
-    'author' => $post->author,
-    'body' => $post->body,
-    'date' => $post->date->format('Y-m-d')
+    'author' => $this->post->author,
+    'body' => $this->post->body,
+    'date' => $this->post->date->format('Y-m-d')
   ]);
 
   $sendRequest()->assertRedirect(route('login'));
@@ -24,28 +27,27 @@ it('will update a blog post when an admin is logged in', function () {
   login();
 
   $sendRequest()
-    ->assertRedirect(action([BlogPostAdminController::class, 'edit'], $post->slug));
+    ->assertRedirect(action([BlogPostAdminController::class, 'edit'], $this->post->slug));
 
-  expect($post->refresh()->title)->toBe('test');
+  expect($this->post->refresh()->title)->toBe('test');
 });
 
 
 it('validates required fields on the post edit form', function () {
   /** @var Illuminate\Foundation\Testing\TestCase $this */
-  $post = BlogPost::factory()->create();
 
   login();
 
-  post(action([BlogPostAdminController::class, 'update'], $post->slug), [])
+  post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [])
     ->assertSessionHasErrors([
       'title', 'author', 'body', 'date'
     ]);
 
-  post(action([BlogPostAdminController::class, 'update'], $post->slug), [
+  post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
     'title' => 'test',
-    'author' => $post->author,
-    'body' => $post->body,
-    'date' => $post->date->format('Y-m-d')
+    'author' => $this->post->author,
+    'body' => $this->post->body,
+    'date' => $this->post->date->format('Y-m-d')
   ])
     ->assertSessionHasNoErrors();
 });
@@ -53,14 +55,13 @@ it('validates required fields on the post edit form', function () {
 
 it('will validate the date format on the edit blog post screen', function () {
   /** @var Illuminate\Foundation\Testing\TestCase $this */
-  $post = BlogPost::factory()->create();
 
   login();
 
-  post(action([BlogPostAdminController::class, 'update'], $post->slug), [
+  post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
     'title' => 'test',
-    'author' => $post->author,
-    'body' => $post->body,
+    'author' => $this->post->author,
+    'body' => $this->post->body,
     'date' => '02/02/2022'
   ])->dumpSession()
     ->assertSessionHasErrors(['date' => 'The date does not match the format Y-m-d.']);
