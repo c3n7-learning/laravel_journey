d35c69fc3ad97f977a5d644c809b94f78803ebc3
Timothy Karani <kartimothy@gmail.com>
2022-07-20 09:49:25 +0300

    Can test middleware

--- testing-laravel-blog-pest/app/Http/Middleware/RedirectMiddleware.php
+++ testing-laravel-blog-pest/app/Http/Middleware/RedirectMiddleware.php
@@ -8,16 +8,16 @@ use Illuminate\Http\Request;
 
 class RedirectMiddleware
 {
-    public function handle(Request $request, Closure $next)
-    {
-        $path = parse_url($request->fullUrl())['path'] ?? '/';
+  public function handle(Request $request, Closure $next)
+  {
+    $path = parse_url($request->fullUrl())['path'] ?? '/';
 
-        $redirect = Redirect::where('from', $path)->first();
+    $redirect = Redirect::where('from', $path)->first();
 
-        if (! $redirect) {
-            return $next($request);
-        }
-
-        return response()->redirectTo($redirect->to);
+    if (!$redirect) {
+      return $next($request);
     }
+
+    return response()->redirectTo($redirect->to);
+  }
 }
---
+++ testing-laravel-blog-pest/tests/Feature/Http/Middleware/RedirectMiddlewareTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Middleware\RedirectMiddleware;
+use App\Models\Redirect;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Pest\Laravel\get;
+
+test('test the middleware in isolation', function () {
+
+  $response = (new RedirectMiddleware())->handle(
+    createRequest('get', '/'),
+    fn () => new Response()
+  );
+
+  expect($response->isRedirect())->toBeFalse();
+
+
+  Redirect::factory()->create([
+    'from' => '/',
+    'to' => '/new-homepage'
+  ]);
+
+  $response = (new RedirectMiddleware())->handle(
+    createRequest('get', '/'),
+    fn () => new Response()
+  );
+
+  expect($response->isRedirect(url('new-homepage')))->toBeTrue();
+});
+
+
+// test the middleware in full app
+it('will perform the right redirects', function () {
+  // Create a route to use in this test.
+  Route::get('my-test-route', fn () => 'ok')
+    ->middleware(RedirectMiddleware::class);
+
+  get('my-test-route')->assertStatus(200);
+
+  Redirect::factory()->create([
+    'from' => '/my-test-route',
+    'to' => '/new-homepage'
+  ]);
+
+  get('my-test-route')->assertRedirect('/new-homepage');
+});
